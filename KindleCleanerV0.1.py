fe9b9462dab73fedfedfb618055196ef75a6dd5d
TabSimone <79592276+TabSimone@users.noreply.github.com>
2022-08-22 20:41:12 +0100

    Now can read a file

--- KindleCleanerV0.1.py
+++ KindleCleanerV0.1.py
@@ -1,1 +1,17 @@
-print("Hello world")
+
+# C:\Users\taban\OneDrive\Documents\Amazon.txt
+
+
+# path = input("Enter your value: ")
+
+# phrase = input("Enter your value: ")
+
+with  open(r'C:\Users\taban\OneDrive\Documents\Amazon.txt') as f:
+    lines = f.readlines()
+
+with open(r'C:\Users\taban\OneDrive\Documents\Amazon.txt') as f:
+    for line in lines:
+        if line.strip("\n") != "La tua evidenziazione alla posizione":
+            f.write(line)
+
+
