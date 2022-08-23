f629ba7e540551ae37c63a8d3f085b66f7f2eaec
alberspa <albericiprogrammazione@gmail.com>
2022-08-23 21:09:20 +0200

    Update KindleCleanerV0.1.py

--- KindleCleanerV0.1.py
+++ KindleCleanerV0.1.py
@@ -6,12 +6,12 @@
 
 # phrase = input("Enter your value: ")
 
-with  open(r'C:\Users\taban\OneDrive\Documents\Amazon.txt') as f:
+with  open(r'C:\Users\SEDD\Downloads\Tribe.txt', encoding="mbcs") as f:
     lines = f.readlines()
 
-with open(r'C:\Users\taban\OneDrive\Documents\Amazon.txt') as f:
+
+
+with open(r'C:\Users\SEDD\Downloads\Tribe.txt', 'w', encoding="mbcs") as f:
     for line in lines:
-        if line.strip("\n") != "La tua evidenziazione alla posizione":
+       if line.strip("\n") == "La tua evidenziazione alla posizione":
             f.write(line)
-
-
