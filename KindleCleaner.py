9b226e30c756a1eb70d38dd041c2bab460d4ad21
TabSimone <79592276+TabSimone@users.noreply.github.com>
2022-08-23 23:21:00 +0200

    Finished first version
    Added file searcher
    There is however default phrase

---
+++ KindleCleaner.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+from tkinter import filedialog
+import os
+
+#No commant in you want to insert manual path 
+#path = input("Enter path: ")
+
+#No comment if you want to change default phrase
+#phrase = input("Enter phrase: ")
+phrase = "tua evidenziazione"
+
+
+#This open up file finder
+root = tk.Tk()
+root.withdraw()
+path = filedialog.askopenfilename()
+
+
+#Open file in python and read it
+with  open(path, encoding="mbcs") as f:
+    lines = f.readlines()
+
+
+#Delete full ine if there is in that line the phrase
+with open(path, 'w', encoding="mbcs") as f:
+    for line in lines:
+        if line.strip("\n").find(phrase) == -1 :
+            f.write(line)
